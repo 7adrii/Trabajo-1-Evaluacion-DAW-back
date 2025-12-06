8a6c5b83a29dca06f0481a9b738dfe7c0dd66d97
7adrii <adriloma007@gmail.com>
2025-12-06 21:10:44 +0100

    getCarById y getCarByName hechos

--- src/controller/cars.js
+++ src/controller/cars.js
@@ -1,10 +1,41 @@
-const {findAllCars } = require('../service/cars');
+const {findAllCars, findCarById, findCarByName, carExistById, carExistByName} = require('../service/cars');
 
 const getCars = (async (req, res) => {
     const cars =  await findAllCars();
     res.status(200).json(cars);
 });
 
+const getCarById = (async (req, res) => {
+    const id = req.params.id;
+
+    if (!await carExistById(id)) {
+        return res.status(404).json({
+            code: 404,
+            title: 'Not-Found',
+            message: `El coche con id ${id} no existe`
+        });
+    }
+
+    const carId = await findCarById(id);
+    res.status(200).json(carId);
+});
+
+const getCarByName = (async (req, res) => {
+    const name = req.params.name;
+
+    if (!await carExistByName(name)) {
+        return res.status(404).json({
+            code: 404,
+            title: 'Not-Found',
+            message: `El coche con nombre ${name} no existe`
+        });
+    }
+
+    const carName = await findCarByName(name);
+});
+
 module.exports = {
-    getCars
+    getCars,
+    getCarById,
+    getCarByName
 };
--- src/service/cars.js
+++ src/service/cars.js
@@ -4,6 +4,32 @@ const findAllCars = (async () => {
     return await db('cars').select('*');
 });
 
+const findCarById = (async (id) => {
+    return await db('cars').select('*').where({id: id}).first();
+});
+
+const findCarByName = (async (name) => {
+    return await db('cars').select('*').where({name: name}).first();
+});
+
+const carExistById = (async (id) => {
+    const car =  await db('cars').select('*').where({id: id}).first();
+    return car != null;
+});
+
+const carExistByName = (async (name) => {
+    const car =  await db('cars').select('*').where({name: name}).first();
+    if(city === undefined){
+        return false;
+    }
+    return true;
+});
+
+
 module.exports = {
-    findAllCars
+    findAllCars,
+    findCarById,
+    findCarByName,
+    carExistById,
+    carExistByName
 };
