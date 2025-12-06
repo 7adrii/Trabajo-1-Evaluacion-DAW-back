b6717be66efffa3475b80c09ae580ce2ab2cbbc8
7adrii <adriloma007@gmail.com>
2025-12-06 23:04:19 +0100

    putCar terminado

--- src/route/cars.js
+++ src/route/cars.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-const { getCars, getCarById, getCarByModelo, postCar } = require('../controller/cars');
+const { getCars, getCarById, getCarByModelo, postCar, putCar } = require('../controller/cars');
 router.get('/cars', getCars);
 router.get('/cars/id/:id', getCarById);
 router.get('/cars/modelo/:modelo', getCarByModelo);
 router.post('/cars', postCar);
+router.put('/cars/id/:id', putCar);
 
 module.exports = router;
